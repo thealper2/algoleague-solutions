453618923d4ca6d04c29b8bcd4e8033bd24c801b
Alper Karaca <thisiseternity12@gmail.com>
2025-11-06 10:13:57 +0300

    Is It A Prime Number (Python Is Forced)

---
+++ 01/is_it_a_prime_number_python_is_forced.py
@@ -0,0 +1,16 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+            
+    return True
+    
+n = int(input())
+result = is_prime(n)
+if result:
+    print('YES')
+else:
+    print('NO')
