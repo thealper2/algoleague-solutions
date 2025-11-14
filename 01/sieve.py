21d0acb22215f852ce0b1dbcc1df2d05dbfe4326
Alper Karaca <thisiseternity12@gmail.com>
2025-11-14 08:19:19 +0300

    Sieve

---
+++ 01/sieve.py
@@ -0,0 +1,10 @@
+n = int(input())
+sieve = [True] * (n + 1)
+sieve[0] = sieve[1] = False
+for i in range(2, int(n ** 0.5) + 1):
+    if sieve[i]:
+        for j in range(i * i, n + 1, i):
+            sieve[j] = False
+            
+            
+print(sum(sieve))
